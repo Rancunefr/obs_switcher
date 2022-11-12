ac9fb09346ba88789bf79aebfe5664c9712fa6ce
Rancune <rancune@rancune.org>
2022-11-12 14:40:53 +0100

    bugfix

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,16 @@ use obws::requests::scene_items::Id;
 async fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 3 {
+    if args.len() != 4 {
         std::process::exit(exitcode::OK);
     }
 
     let arg_scene = &args[1];
     let arg_source = &args[2];
+    let str_enable: bool = args[3].eq("true");
 
     let client = Client::connect("localhost", 4455, Some("totototo")).await?;
     
-    let scenes = client.scenes();
-    let current_scene = scenes.current_program_scene().await?;
-
     let scene_items = client.scene_items();
     let item_id = scene_items
         .id(Id {
@@ -31,9 +29,9 @@ async fn main() -> Result<()> {
 
     scene_items
         .set_enabled(SetEnabled {
-            scene: &current_scene,
+            scene: arg_scene,
             item_id: item_id,
-            enabled: !scene_items.enabled( &current_scene, item_id ).await?,
+            enabled: str_enable,
         })
         .await?;
 
